5233414897e794b81657dcf2876cdb863a43e1a0
Q118 <shelbyfish91@gmail.com>
2020-07-02 20:56:32 -0700

    changed contents prompt to a checkbox

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 //sections: title, description, table of contents, installation,
 // usage, license, badges, contributing , Tests, Questions
+
+main();
 
 async function main() {
 	try {
 		const answers = await generateUserInfo();
 		const html = renderHTML(answers);
-		await writeFileAsync("README.md", html);
-		console.log("created README.md!");
+		await writeFileAsync("READMEtest.md", html);
+		console.log("created READMEtest.md!");
 	} catch (error) {
 		console.log(error);
 	}
@@ -31,10 +36,25 @@ function generateUserInfo() {
 			name: "description",
 		},
 		{
-			type: "input",
+			type: "checkbox",
 			message:
-				"List which section you would like linked in your table of contents. (e.g. installation, usage, license, questions.)",
-			name: "contents",
+				"Select which sections you would like in your table of conents, as many as you like.",
+            name: "contents",
+            choices: [
+                "title",
+                "description",
+                "contents",
+                "installation",
+                "usage",
+                "license",
+                "badgeLabel",
+                "badgeMessage",
+                "badgeColor",
+                "contributing",
+                "tests",
+                "username",
+            ]
+            
 		},
 		{
 			type: "input",
@@ -118,6 +138,20 @@ function renderHTML({
 	tests,
 	username,
 }) {
-return ' '
+return `
+    title: ${title}
+	description: ${description}
+	contents:${contents}
+	installation:${installation}
+	usage: ${usage}
+	license: ${license}
+ 	badgeLabel: ${badgeLabel}
+	badgeMessage: ${badgeMessage}
+	badgeColor: ${badgeColor}
+	contributing: ${contributing}
+	tests: ${tests}
+	username: ${username}
+
+`;
 
 }
